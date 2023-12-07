861f4eb355d312daf9c6fbffbace54550e6a7fea
DaryaMarkova <darya.towarddreams@yandex.ru>
2023-12-07 15:32:44 +0300

    continue conditional types

--- types-manipulation/lecture.ts
+++ types-manipulation/lecture.ts
@@ -13,3 +13,54 @@ const key: KeysOfUser = "name";
 
 const mUser: IUser = { name: "Вася", age: 30 };
 const userName = getValue(mUser, "name");
+
+/** typeof */
+const _user = { name: "Вася" };
+type keyOfUser = keyof typeof _user;
+
+enum Direction {
+  Up,
+  Down,
+}
+type d = keyof typeof Direction;
+
+/* index types */
+
+interface RoleX {
+  name: string;
+}
+
+interface UserX {
+  name: string;
+  roles: RoleX[];
+}
+
+const user: UserX = {
+  name: "Вася",
+  roles: [],
+};
+
+const roleNames = "roles";
+
+const nameUser = user["name"];
+type roleType = UserX["roles"];
+type roleType2 = UserX[typeof roleNames];
+
+/* Conditional Types */
+
+interface HTTPResponse<T extends "success" | "failed"> {
+  code: number;
+  data: T extends "success" ? string : Error;
+}
+
+class UserPersistend {
+  dbId: string;
+}
+
+function getUser(dbIdOrId: string | number): UserX | UserPersistend {
+  if (typeof dbIdOrId === "number") {
+    return { name: "Darya", roles: [] };
+  } else {
+    return new UserPersistend();
+  }
+}
