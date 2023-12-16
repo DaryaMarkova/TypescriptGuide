6635b11917b954ec62e8ce55ba1b42b032d51719
DaryaMarkova <darya.towarddreams@yandex.ru>
2023-12-16 16:16:33 +0300

    decorator examples

--- decorators/lecture.ts
+++ decorators/lecture.ts
@@ -7,12 +7,19 @@ interface IUserService {
 @setUsers(2)
 // @threeAdvanced
 class UserService implements IUserService {
-  usersCount: number;
+  @Max(100)
+  usersCount: number; /* target: UserService, propertyKey: usersCount */
 
   getUsersInDatabase(): number {
     return this.usersCount;
   }
 }
+
+function methodLog(
+  target: Object,
+  propertyKey: string | symbol,
+  descriptor: TypedPropertyDescriptor<(...args: any[]) => any>
+) {}
 
 function threeUserAdvanced<T extends { new (...args: any[]) }>(constructor: T) {
   return class extends constructor {
@@ -30,6 +37,42 @@ function log() {
   console.log("log init");
   return (target: Function) => {
     console.log("setUsers run");
+  };
+}
+
+// декоратор метода
+function Log(
+  target: Object /* Объект, к которому относится метод */,
+  propertyKey: string | symbol /* Название метода */,
+  descriptor: TypedPropertyDescriptor<
+    (args: any[]) => void
+  > /* Конфигурация объекта: {value, writable,enumerable, configurable} */
+) {
+  descriptor.value = () => {
+    // подменяем значение, которое возвращает новый метод
+    console.log("handle error");
+  };
+}
+
+// декоратор свойства
+function Max(max: number) {
+  return (target: Object, propertyKey: string | symbol) => {
+    let value: number;
+
+    const setter = function (newValue: number) {
+      if (newValue > max) {
+        console.log(`Нельзя установить значение больше ${max}`);
+      }
+    };
+
+    const getter = function () {
+      return value;
+    };
+
+    Object.defineProperty(target, propertyKey, {
+      set: setter,
+      get: getter,
+    });
   };
 }
 
--- decorators/task.ts
+++ decorators/task.ts
@@ -4,3 +4,29 @@ function CreatedAt<T extends { new (...args: any[]): {} }>(constructor: T) {
     createdAt = new Date();
   };
 }
+
+/* декоратор, который отлавливает исходящие ошибки у метода*/
+function catchError({ rethrow: boolean } = { rethrow: true }) {
+  return (
+    target: Object,
+    _: string | symbol,
+    descriptor: TypedPropertyDescriptor<(args: any[]) => void>
+  ): TypedPropertyDescriptor<(args: any[]) => any> | void => {
+    const method = descriptor.value;
+
+    descriptor.value = async (...args: any[]) => {
+      try {
+        const res = await method?.apply(target, args);
+        return res;
+      } catch (e) {
+        if (rethrow) {
+          throw e;
+        } else {
+        }
+      }
+    };
+  };
+}
+
+// пример использования
+// @catchError({ rethrow: true })
